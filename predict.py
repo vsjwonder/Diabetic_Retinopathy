19c5f82bda48bbb9ec2ab2d31ca6110fa31b3497
Dr. Vivek J <61494546+vsjwonder@users.noreply.github.com>
2021-10-28 17:44:37 +0530

    TF Lite optimised model

--- predict.py
+++ predict.py
@@ -9,6 +9,8 @@ from tensorflow.keras.models import load_model
 
 from PIL import Image
 import numpy as np
+import tensorflow as tf
+from time import time
 
 
 def load(filename):
@@ -26,13 +28,50 @@ class retinopathy:
 
     def predictionretinopathy(self):
         # load model
-        model = load_model('retina_weights.hdf5')
+        #model = load_model('retina_weights.hdf5')
+        # Change to bellow
+        #tflite_model_path ='retina.tflite'
+        tflite_model_path ='retina_opt.tflite'
+
+        ######## For tflite ######
+        # Load the TFLite model and allocate tensors.
+        interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
+        interpreter.allocate_tensors()
+
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        # Test the model on input data.
+        input_shape = input_details[0]['shape']
+        ######## tflite end ######
 
         # summarize model
         # model.summary()
         imagename = self.filename
         test_image = load(imagename)
-        result = model.predict(test_image)
+
+        ####For TFlite
+        # If tflite model used
+        input_data = test_image
+
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+
+        time_before = time()
+        interpreter.invoke()
+        time_after = time()
+        total_tflite_time = time_after - time_before
+        # print("Total prediction time for tflite without opt model is: ", total_tflite_time)
+
+        output_data_tflite = interpreter.get_tensor(output_details[0]['index'])
+        # print("The tflite w/o opt prediction for this image is: ", output_data_tflite)
+
+
+        # change result to interpreter for tflite
+
+        #result = model.predict(test_image)
+        # or #
+        result = interpreter.get_tensor(output_details[0]['index'])
         result = np.argmax(result)
 
         if result == 0:
